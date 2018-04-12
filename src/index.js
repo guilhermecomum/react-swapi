3b5af850a1d0ff92a6e4784fa33fd34c47253862
Guilherme Guerra <guerrinha@comum.org>
2018-04-12 09:33:19 -0300

    Sets up deploy and removes unecessary code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
+import store from './store'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
       <App />
-    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
--- src/store/rootReducers.js
+++ src/store/rootReducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 
 import app from '../reducer'
 
 const reducers = {
-  routing: routerReducer,
   app
 }
 
