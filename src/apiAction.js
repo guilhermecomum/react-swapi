bf3787066eae272d503e84ef26b7784963b60c8c
Guilherme Guerra <guilherme.ga@gmail.com>
2022-09-29 16:03:56 -0300

    Fix api url

--- src/apiAction.js
+++ src/apiAction.js
@@ -1,102 +1,99 @@
-export const requestHeaders = credentials => ({
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-})
+export const requestHeaders = (credentials) => ({
+  Accept: "application/json",
+  "Content-Type": "application/json",
+});
 
-export const parseError = error => {
+export const parseError = (error) => {
   if (error instanceof Error) {
     return {
       id: error.name,
       message: error.message,
-    }
+    };
   }
 
   if (error.errors) {
-    const messages = error.errors.fullMessages || error.errors
-    const message = messages[0]
+    const messages = error.errors.fullMessages || error.errors;
+    const message = messages[0];
     return {
       id: error.id || error.errors.id || message,
       message,
       error,
-    }
+    };
   }
 
-  return error
-}
+  return error;
+};
 
-export const apiAction = options => (dispatch, getState) => {
-  return apiDispatchAction(dispatch, options, getState)
-}
-
+export const apiAction = (options) => (dispatch, getState) => {
+  return apiDispatchAction(dispatch, options, getState);
+};
 
 export const apiDispatchAction = async (dispatch, options, getState) => {
-  const { prefix, path, method, params } = options
-  const { requestAttributes, processResponse, successAction } = options
+  const { prefix, path, method, params } = options;
+  const { requestAttributes, processResponse, successAction } = options;
 
   dispatch({
     type: `${prefix}_REQUEST`,
     ...requestAttributes,
-  })
+  });
 
-  let fetchOptions = { method: method ? method : 'get' }
+  let fetchOptions = { method: method ? method : "get" };
 
   fetchOptions.headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
   if (params) {
-    fetchOptions.body = JSON.stringify(params)
+    fetchOptions.body = JSON.stringify(params);
   }
 
-  fetchOptions.mode = 'cors'
+  fetchOptions.mode = "cors";
 
-  return fetch(`https://swapi.co/api${path}`, fetchOptions).then(
-    async response => {
-      const body = await response.text()
-      const contentType = response.headers.get('content-type')
-      let json
+  return fetch(`https://swapi.dev/api${path}`, fetchOptions).then(
+    async (response) => {
+      const body = await response.text();
+      const contentType = response.headers.get("content-type");
+      let json;
 
       if (contentType && contentType.match(/application\/json/)) {
-        json = JSON.parse(body)
+        json = JSON.parse(body);
       } else {
         json = {
           id: `${response.status}`,
           message: body,
-        }
+        };
       }
 
       if (response.ok) {
         dispatch({
           type: `${prefix}_SUCCESS`,
           ...(processResponse && (await processResponse(response, json))),
-        })
+        });
 
         if (successAction) {
-          await successAction(dispatch)
+          await successAction(dispatch);
         }
-
       } else {
-
         return dispatch({
           type: `${prefix}_FAILURE`,
           ...requestAttributes,
           error: parseError(json),
-        })
+        });
       }
     },
-    error => {
-      if (process.env.NODE_ENV === 'test') {
-        console.warn(error)
+    (error) => {
+      if (process.env.NODE_ENV === "test") {
+        console.warn(error);
       }
 
       dispatch({
         type: `${prefix}_FAILURE`,
         ...requestAttributes,
         error: parseError(error),
-      })
-    },
-  )
-}
+      });
+    }
+  );
+};
 
-export default apiAction
+export default apiAction;
